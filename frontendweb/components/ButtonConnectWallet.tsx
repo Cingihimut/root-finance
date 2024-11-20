48d570224e374bfc6ba1df5f996847adcc460ca4
glunoty <iluviums@gmail.com>
2024-11-21 04:50:55 +0700

    🪖 configure rainbowkit button connect

---
+++ frontendweb/components/ButtonConnectWallet.tsx
@@ -0,0 +1,95 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { Button } from "./ui/button";
+import Image from "next/image";
+
+function ButtonConnectWallet() {
+  return (
+    <ConnectButton.Custom>
+      {({
+        account,
+        chain,
+        openAccountModal,
+        openChainModal,
+        openConnectModal,
+        authenticationStatus,
+        mounted,
+      }) => {
+        // Note: If your app doesn't use authentication, you
+        // can remove all 'authenticationStatus' checks
+        const ready = mounted && authenticationStatus !== "loading";
+        const connected =
+          ready &&
+          account &&
+          chain &&
+          (!authenticationStatus || authenticationStatus === "authenticated");
+
+        return (
+          <div
+            {...(!ready && {
+              "aria-hidden": true,
+              style: {
+                opacity: 0,
+                pointerEvents: "none",
+                userSelect: "none",
+              },
+            })}
+          >
+            {(() => {
+              if (!connected) {
+                return (
+                  <Button
+                    onClick={openConnectModal}
+                    type="button"
+                    className="font"
+                  >
+                    Connect
+                  </Button>
+                );
+              }
+
+              if (chain.unsupported) {
+                return (
+                  <Button onClick={openChainModal} type="button">
+                    Wrong network
+                  </Button>
+                );
+              }
+
+              return (
+                <div className="flex gap-2">
+                  <Button
+                    onClick={openChainModal}
+                    variant="secondary"
+                    size="icon"
+                  >
+                    {chain.hasIcon && (
+                      <div
+                        className="w-5 h-5 rounded-full overflow-hidden"
+                        style={{ background: chain.iconBackground }}
+                      >
+                        {chain.iconUrl && (
+                          <Image
+                            alt={"?"}
+                            src={chain.iconUrl}
+                            width={100}
+                            height={100}
+                          />
+                        )}
+                      </div>
+                    )}
+                  </Button>
+
+                  <Button onClick={openAccountModal} variant={"secondary"}>
+                    {account.displayName}
+                  </Button>
+                </div>
+              );
+            })()}
+          </div>
+        );
+      }}
+    </ConnectButton.Custom>
+  );
+}
+
+export default ButtonConnectWallet;
